c712cbc0e5dd80b0837a0094f3b5b1b615c9a468
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2023-03-19 03:08:31 +0100

    Add PWA support

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,36 @@
 <script>
   import "../app.css";
+  import { onMount } from 'svelte'
+  import { pwaInfo } from 'virtual:pwa-info'
+
+  onMount(async () => {
+    if (pwaInfo) {
+      const { registerSW } = await import('virtual:pwa-register')
+      registerSW({
+        immediate: true,
+        onRegistered(r) {
+          // uncomment following code if you want check for updates
+          // r && setInterval(() => {
+          //    console.log('Checking for sw update')
+          //    r.update()
+          // }, 20000 /* 20s for testing purposes */)
+          console.log(`SW Registered: ${r}`)
+        },
+        onRegisterError(error) {
+          console.log('SW registration error', error)
+        }
+      })
+    }
+  })
+  
+  $: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : ''
+
+  console.log(pwaInfo)
 </script>
+
+<svelte:head>
+    {@html webManifest}
+</svelte:head>
 
 <main class="flex flex-col justify-center items-center h-screen w-screen">
   <slot />
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,32 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+      includeAssets: ['/favicon.png'],
+      manifest: {
+        name: 'My Weight Tracker',
+        start_url: "/",
+        short_name: 'Weight Tracker',
+        description: 'My Weight Tracker',
+        theme_color: '#18181b',
+				background_color: '#18181b',
+        icons: [
+          {
+            src: '/favicon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/favicon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+	]
 });
